9c63c61f6565e7954bbd91ca79d0fe6ff527dfa6
Dominik Slanitz <dominik.slanitz@student.tugraz.at>
2018-03-21 09:46:04 +0100

    init android frontend

---
+++ android-frontend/app/src/main/java/xpmxp1/tenderstar/MainActivity.java
@@ -0,0 +1,13 @@
+package xpmxp1.tenderstar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
