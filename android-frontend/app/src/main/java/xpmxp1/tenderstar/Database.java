b5d814c5e32c427029a9df073b79088354a10e88
Sebastian Straussberger <sebastian.straussberger@student.tugraz.at>
2018-04-25 10:53:07 +0200

    [RH,SS] Added dummie data for first release

--- android-frontend/app/src/main/java/xpmxp1/tenderstar/Database.java
+++ android-frontend/app/src/main/java/xpmxp1/tenderstar/Database.java
@@ -44,15 +44,27 @@ public class Database {
     private void createDummies() {
         // create products
         products = new ArrayList<>();
-        products.add(new Product("Milk", Product.Category.FOOD, "descr", 2.0f));
-        products.add(new Product("Water", Product.Category.FOOD,"descr", 2.0f));
-        products.add(new Product("Cereal", Product.Category.FOOD, "descr", 2.0f));
+        products.add(new Product("Milk", Product.Category.FOOD, "descr", 1.2f));
+        products.add(new Product("Water", Product.Category.FOOD,"descr", 0.8f));
+        products.add(new Product("Cereal", Product.Category.FOOD, "descr", 3.8f));
         products.add(new Product("Sausage", Product.Category.FOOD, "descr", 2.0f));
-        products.add(new Product("Pizza", Product.Category.FOOD, "descr", 2.0f));
+        products.add(new Product("Pizza", Product.Category.FOOD, "descr", 4.4f));
         products.add(new Product("Rice", Product.Category.FOOD, "descr", 2.0f));
-        products.add(new Product("Beer", Product.Category.ALCOHOL, "descr", 2.0f));
-        products.add(new Product("Wine", Product.Category.ALCOHOL, "descr", 2.0f));
-        products.add(new Product("Whiskas", Product.Category.CATFOOD, "descr", 2.0f));
+        products.add(new Product("Beer", Product.Category.ALCOHOL, "descr", 1.0f));
+        products.add(new Product("Wine", Product.Category.ALCOHOL, "descr", 7.0f));
+        products.add(new Product("Vodka", Product.Category.ALCOHOL, "descr", 32.7f));
+        products.add(new Product("Whisky", Product.Category.ALCOHOL, "descr", 99.99f));
+        products.add(new Product("Felix", Product.Category.CATFOOD, "delicious cat food", 1.5f));
+        products.add(new Product("Sheba", Product.Category.CATFOOD, "cat food", 1.25f));
+        products.add(new Product("Whiskas", Product.Category.CATFOOD, "descr", 1.3f));
+        products.add(new Product("Ketchup", Product.Category.FOOD, "descr", 2.8f));
+        products.add(new Product("InstantNoodles", Product.Category.FOOD, "descr", 3.1f));
+        products.add(new Product("Pasta", Product.Category.FOOD, "descr", 1.7f));
+        products.add(new Product("Cheese", Product.Category.FOOD, "descr", 1.3f));
+        products.add(new Product("Ham", Product.Category.FOOD, "descr", 2.0f));
+        products.add(new Product("Rum", Product.Category.ALCOHOL, "descr", 4.7f));
+        products.add(new Product("Alami", Product.Category.CATFOOD, "descr", 11.6f));
+        products.add(new Product("Gourmet", Product.Category.CATFOOD, "descr", 16.9f));
 
         // create stores
         stores = new ArrayList<>();
@@ -60,5 +72,6 @@ public class Database {
         stores.add(new Store("Spar", new Store.OpeningHours(new Store.Time(), new Store.Time(), false), "Straße 2", "8010"));
         stores.add(new Store("Penny", new Store.OpeningHours(new Store.Time(), new Store.Time(), false), "Straße 3", "8010"));
         stores.add(new Store("Lidl", new Store.OpeningHours(new Store.Time(), new Store.Time(), false), "Straße 4", "8010"));
+        stores.add(new Store("Zoo4You", new Store.OpeningHours(new Store.Time(), new Store.Time(), false), "Straße 4", "8010"));
     }
 }
