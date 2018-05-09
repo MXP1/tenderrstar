01223030afb696e50c82a97277e1cfcb9d3bf5aa
Dominik Slanitz <dominik.slanitz@student.tugraz.at>
2018-05-09 08:32:44 +0200

    [DS, TL] fixed label

--- android-frontend/app/src/main/java/xpmxp1/tenderstar/ShoppingCartFragment.java
+++ android-frontend/app/src/main/java/xpmxp1/tenderstar/ShoppingCartFragment.java
@@ -77,8 +77,9 @@ public class ShoppingCartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        Log.d("TEST", Integer.toString(Database.getInstance().getShoppingCartProducts().size()));
+        if (mListener != null) {
+            mListener.onFragmentInteraction(Uri.parse("Shopping Cardy"));
+        }
 
         // Create List view
         View view = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
