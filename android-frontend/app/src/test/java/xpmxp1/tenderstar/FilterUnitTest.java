ab8aa663693531a8e5b00d9fe5a9f22b6d8e24b9
Unknown <baran.meric@student.tugraz.at>
2018-05-23 12:11:08 +0200

    [BaM, DI] Added FilterUnitTest

--- android-frontend/app/src/test/java/xpmxp1/tenderstar/FilterUnitTest.java
+++ android-frontend/app/src/test/java/xpmxp1/tenderstar/FilterUnitTest.java
@@ -1,187 +1,113 @@
 package xpmxp1.tenderstar;
 
-import org.junit.Before;
-import org.junit.Test;
-
+import android.app.Application;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Date;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import xpmxp1.tenderstar.app_objects.Customer;
+import xpmxp1.tenderstar.app_objects.Favorit;
+import xpmxp1.tenderstar.app_objects.OpeningHours;
+import xpmxp1.tenderstar.app_objects.Product;
+import xpmxp1.tenderstar.app_objects.ProductCategory;
+import xpmxp1.tenderstar.app_objects.ProductRating;
+import xpmxp1.tenderstar.app_objects.SavedOffer;
+import xpmxp1.tenderstar.app_objects.Store;
+import xpmxp1.tenderstar.app_objects.StoreRating;
+import xpmxp1.tenderstar.app_objects.StoreType;
 import xpmxp1.tenderstar.app_objects.Product;
 import xpmxp1.tenderstar.app_objects.Store;
 import xpmxp1.tenderstar.app_objects.Tag;
-
-import static org.junit.Assert.*;
-
+import xpmxp1.tenderstar.app_objects.Tag;
+import xpmxp1.tenderstar.database.TenderstarDB;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class FilterUnitTest {
+
+public class FilterUnitTest{
+
+    List<Store> stores = new ArrayList<>();
     List<Product> products = new ArrayList<>();
-    Filter filter;
-
-    Tag tagCat = new Tag("Cat");
-    Tag tagAnimal = new Tag("Animal");
-    Tag tagVegetable = new Tag("Vegetable");
-    Tag tagFruit = new Tag("Fruit");
-    Tag tagSpirits = new Tag("Spirits");
-
-    Store storeBilla = new Store("Billa");
-    Store storeSpar = new Store("Spar");
-    Store storePenny = new Store("Penny");
+    Filter filter_products;
+    Filter filter_stores;
 
     @Before
     public void initialize() {
-        initProducts();
+        initStoresProducts();
     }
 
-    public void initProducts() {
-        Product tempProduct;
-        products = new ArrayList<>();
-
-        tempProduct = new Product("Whiskey", Product.Category.ALCOHOL,"descr", 2.0f);
-        tempProduct.tags.add(tagSpirits);
-        products.add(tempProduct);
-
-        tempProduct = new Product("Jack Daniels", Product.Category.ALCOHOL, "descr", 2.0f);
-        tempProduct.tags.add(tagSpirits);
-        products.add(tempProduct);
-
-        tempProduct = new Product("Banana", Product.Category.FOOD, "descr", 2.0f);
-        tempProduct.tags.add(tagFruit);
-        products.add(tempProduct);
-
-        tempProduct = new Product("Whiskas", Product.Category.CATFOOD, "descr", 2.0f);
-        tempProduct.tags.add(tagAnimal);
-        tempProduct.tags.add(tagCat);
-        products.add(tempProduct);
-
-        tempProduct = new Product("Carrot", Product.Category.FOOD, "descr", 2.0f);
-        tempProduct.tags.add(tagVegetable);
-        products.add(tempProduct);
-
-        tempProduct = new Product("Paprika", Product.Category.FOOD, "descr", 2.0f);
-        tempProduct.tags.add(tagVegetable);
-        products.add(tempProduct);
 
 
-        filter = new Filter(products);
+    public void initStoresProducts(){
+        StoreType st1 = new StoreType("Food");
+        StoreType st2 = new StoreType("Tech");
+
+        Store store1 = new Store("Billa", "Billa", "Billa-Store", "ex-link", st1.getId(),
+                new OpeningHours(new OpeningHours.Time(), new OpeningHours.Time(), false), "Main Street 7", "1010");
+
+        Store store2 = new Store("Spar", "Spar", "Spar-Store", "ex-link", st1.getId(),
+                new OpeningHours(new OpeningHours.Time(), new OpeningHours.Time(), false), "Tech Street 1", "1010");
+
+        Store store3 = new Store("Saturn", "Saturn", "Saturn-Store", "ex-link", st2.getId(),
+                new OpeningHours(new OpeningHours.Time(), new OpeningHours.Time(), false), "Test Street 192", "8010");
+
+        stores.add(store1);
+        stores.add(store2);
+        stores.add(store3);
+
+        ProductCategory pc1 = new ProductCategory("Food");
+        ProductCategory pc2 = new ProductCategory("Tech");
+
+        Product temp1 = new Product("Butter", "desc", 2.0, 10.0, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis() + 10000), store1.getId(), pc1.getId());
+        Product temp2 = new Product("Egg", "desc",  1.3, 5.0, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis() + 10000), store1.getId(), pc1.getId());
+        Product temp3 = new Product("Milk", "desc",699.99, 25.0, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis() + 10000), store2.getId(), pc1.getId());
+        Product temp4 = new Product("Laptop", "desc", 1229.79, 12.5, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis() + 10000), store3.getId(), pc2.getId());
+
+        products.add(temp1);
+        products.add(temp2);
+        products.add(temp3);
+        products.add(temp4);
+
+        filter_products = new Filter(products);
+        filter_stores = new Filter(stores, 1);
     }
+
 
     @Test
-    public void testCategory() throws Exception {
-        initProducts();
-
-        filter.setCategory(Product.Category.FOOD);
-        List<Product> results = filter.results();
-        System.out.print(products.size());
-        assertEquals("Banana", results.get(0).name);
-    }
-
-    @Test
-    public void testTags1() throws Exception {
-        initProducts();
-
-        filter.addTag(tagVegetable);
-        List<Product> results = filter.results();
-
-        assertEquals(2, results.size());
-        assertEquals("Carrot", results.get(0).name);
-    }
-
-    @Test
-    public void testTags2() throws Exception {
-        filter.addTag(tagCat);
-        filter.addTag(tagAnimal);
-        List<Product> results = filter.results();
-
-        assertEquals(1, results.size());
-        assertEquals("Whiskas", results.get(0).name);
-    }
-
-    @Test
-    public void testStore() throws Exception {
-        filter.addStore(storeBilla);
-        List<Product> results = filter.results();
-
-        assertEquals(3, results.size());
-    }
-
-    @Test
-    public void testSearchString1() throws Exception {
-        filter.setSearchString("Banana");
-        List<Product> results = filter.results();
-
-        assertEquals("Banana", results.get(0).name);
-    }
-
-    @Test
-    public void testSearchString2() throws Exception {
-        filter.setSearchString("Apple");
-        List<Product> results = filter.results();
+    public void testSearchProduct1() throws Exception {
+        filter_products.setSearchString("Apple");
+        List<Product> results = filter_products.results();
 
         assertEquals(0, results.size());
     }
 
     @Test
-    public void testSearchString3() throws Exception {
-        filter.setSearchString("Whiskey Whiskas Banana");
-        List<Product> results = filter.results();
-
-        assertEquals(3, results.size());
-    }
-
-    @Test
-    public void testSearchString4() throws Exception {
-        filter.setSearchString("Banana Whiskas ");
-        List<Product> results = filter.results();
-
-        assertEquals(2, results.size());
-    }
-
-    @Test
-    public void testSearchString5() throws Exception {
-        filter.setSearchString("whiskey BANANA");
-        List<Product> results = filter.results();
-
-        assertEquals(2, results.size());
-    }
-
-    @Test
-    public void testSearchString6() throws Exception {
-        filter.setSearchString("Jack Daniels Banana");
-        List<Product> results = filter.results();
-
-        assertEquals(2, results.size());
-        assertEquals("Jack Daniels", results.get(0).name);
-    }
-
-    @Test
-    public void testCombination1() throws Exception {
-        filter.setSearchString("Jack Daniels Banana");
-        filter.addTag(tagSpirits);
-        List<Product> results = filter.results();
+    public void testSearchProduct2() throws Exception {
+        filter_products.setSearchString("Butter");
+        List<Product> results = filter_products.results();
 
         assertEquals(1, results.size());
-        assertEquals("Jack Daniels", results.get(0).name);
     }
 
     @Test
-    public void testCombination2() throws Exception {
-        filter.setSearchString("Jack Daniels Banana");
-        filter.addTag(tagSpirits);
-        List<Product> results = filter.results();
+    public void testSearchStore1() throws Exception {
+        filter_stores.setSearchString("Saturn");
+        List<Store> results = filter_stores.resultsStore();
 
         assertEquals(1, results.size());
-        assertEquals("Jack Daniels", results.get(0).name);
     }
 
     @Test
-    public void testCombination3() throws Exception {
-        filter.setSearchString("Jack Daniels");
-        List<Product> results = filter.results();
+    public void testSearchStore2() throws Exception {
+        filter_stores.setSearchString("Hofer");
+        List<Store> results = filter_stores.resultsStore();
 
         assertEquals(0, results.size());
     }
